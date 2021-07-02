4f62f0c8d62bb773898c285795d0203379768f9c
CapuT-T <Rodrigo@gmail.com>
2021-07-02 16:04:09 -0500

    Nova Versão do Scroll Anima

--- js/modules/sroll-anima.js
+++ js/modules/sroll-anima.js
@@ -3,23 +3,40 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (isSectionVisible) section.classList.add('ativo');
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const sectionTop = section.offsetTop;
 
-      else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+      return {
+        element: section,
+        offsetTop: Math.floor(sectionTop - this.windowMetade),
+      };
+    });
+  }
+
+  checkDistance() {
+    const scroll = window.pageYOffset;
+    this.distance.forEach((item) => {
+      if (scroll > item.offsetTop) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
